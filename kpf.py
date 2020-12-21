ff8060a1fbf1391469b4dffcd5899734a28edc17
IsmaBH <deigo-b@hotmail.com>
2020-12-20 21:59:43 -0600

    Primer algoritmo greedy Se termino la implementación del algoritmo greedy para el problema de la mochila fraccionaria

--- kpf.py
+++ kpf.py
@@ -14,10 +14,35 @@ class objeto:
     def getCosto(self):
         return self.__costo
 
-def ordenamiento(listaObjetos):
-    resultado = listaObjetos
-    resultado.sort(key=getCosto())
-    return resultado
-
+#Funcion que implementa el algoritmo de la mochila fraccionaria
+def kpf(listaObjetos,pT):
+    listaObjetos.sort(key = lambda x: x.getCosto(),reverse = True)
+    espacioRestante = pT
+    solucion = []
+    for i in range(len(listaObjetos)):
+        if (listaObjetos[i].getPeso() > espacioRestante):
+            fraccion = espacioRestante / listaObjetos[i].getPeso()
+            objeto = (listaObjetos[i].getIndice(),listaObjetos[i].getPeso() * fraccion,listaObjetos[i].getValor() * fraccion)
+            solucion.append(objeto)
+        else:
+            objeto = (listaObjetos[i].getIndice(),listaObjetos[i].getPeso(),listaObjetos[i].getValor())
+            solucion.append(objeto)
+            espacioRestante -= listaObjetos[i].getPeso()
+    return solucion
 #Codigo de prueba del algoritmo
-print("Hola soy un mensaje")
+print("Introduce las cadenas de pesos y valores en ese orden")
+p = [int(x) for x in input().split()]
+v = [int(y) for y in input().split()]
+print("Introduce el peso maximo de la mochila")
+pT = int(input())
+#Se construye la lista de objetos
+listaObjetos = []
+for i in range(len(p)):
+    listaObjetos.append(objeto(i+1,v[i],p[i]))
+#Hacemos uso del algoritmo de la mochila
+resultado = kpf(listaObjetos,pT)
+#Imprimimos el resultado
+print("Los objetos elegidos son ", end = "")
+print(resultado)
+#Imprimimos el valor obtenido
+print("El valor total obtenido es: " + str(sum([valor[2] for valor in resultado])))
